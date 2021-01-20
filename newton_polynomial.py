515b1d61f82f7b34d7fc17e138c797e2131ec063
PiosDamian <KHdQUMLrDy4pp9uncUc5PrMTgdWLk2mQnLghLXX2>
2021-01-20 23:04:46 +0100

    not working newton

--- newton_polynomial.py
+++ newton_polynomial.py
@@ -30,7 +30,52 @@ def draw_graph_newton():
         index = index + 1
 
     # wyliczanie i rysowanie interpolacji
+    for point in newton_interpolation(initial_points, field_end):
+        f2.plot(point['x'], point['y'])
+
+
+def test():
+    gd = graph(title='Interpolacja Newtona\'a')
+    f1 = gcurve(graph=gd, color=color.cyan)
+    dots = gdots(graph=gd, color=color.red)
+    f2 = gcurve(graph=gd, color=color.green)
+
+    points = [{'x': 1, 'y': 0.5}, {'x': 1.1, 'y': 0.6}, {'x': 1.2, 'y': 0.3}]
+    f1.plot(0.7, .6)
+    for point in points:
+        f1.plot(point['x'], point['y'])
+        dots.plot(point['x'], point['y'])
+    f1.plot(1.3, 0.2)
+    interpolation = newton_interpolation(points, 1.4)
+    for point in interpolation:
+        f2.plot(point['x'], point['y'])
+    print(interpolation)
+
+
+def newton_interpolation(points, field_end):
+    new_points = []
+    for i in arange(0, field_end, 0.1):
+        x = round(i, 5)
+        new_points.append({'x': x, 'y': multiple_summation(points)})
+    return new_points
+
+
+def multiple_summation(points):
+    result = points[0]['y']
+    for index in range(1, len(points)):
+        point = points[index]
+        result = result + point['y'] * multiple_multiplication(points, point['x'], index)
+    return result
+
+
+def multiple_multiplication(points, x, field_end):
+    result = 1
+    for index in range(0, field_end - 1):
+        xj = points[index]['x']
+        result = result * (x - xj)
+    return result
 
 
 if __name__ == '__main__':
-    draw_graph_newton()
+    # draw_graph_newton()
+    test()
