9c8d30aa28a884c3b554e2a7b5eb1e5c5c089778
PiosDamian <KHdQUMLrDy4pp9uncUc5PrMTgdWLk2mQnLghLXX2>
2021-01-24 10:43:11 +0100

    labels, flexibility

--- zad4/pi.py
+++ zad4/pi.py
@@ -4,40 +4,45 @@ from numpy.ma import arange
 from vpython import graph, gcurve, gdots, color, sqrt, label
 
 
-def draw_point():
-    return random(), random()
+def draw_point(factor=1):
+    return random() * factor, random() * factor
 
 
 def count():
-    gd = graph(ymin=0, ymax=1.1, xmin=0, xmax=1.1)
+    # algroytm działa także dla innych wielkości kwadratu r=a/2
+    r = 0.5
+    gd = graph(ymin=0, ymax=2 * r + 0.5, xmin=0, xmax=2 * r + 0.5)
     f1 = gcurve(graph=gd, color=color.cyan)
     f2 = gcurve(graph=gd, color=color.cyan)
     dots = gdots(graph=gd, color=color.red, radius=2)
-    dots.plot(0.5, 0.5)
-    for x in arange(0, 1.001, 0.01):
-        f1.plot(x, circle_point_lower(x))
-        f2.plot(x, circle_point_upper(x))
+
+    for x in arange(0, 2 * r + 0.001, 0.01):
+        f1.plot(x, circle_point_lower(x, r))
+        f2.plot(x, circle_point_upper(x, r))
 
     points_count = randrange(200, 300)
     ok_points = 0
     while x <= points_count:
         x = x + 1
-        point = draw_point()
+        point = draw_point(2 * r)
         dots.plot(point[0], point[1])
-        if circle_point_upper(point[0]) >= point[1] >= circle_point_lower(point[0]):
+        if circle_point_upper(point[0], r) >= point[1] >= circle_point_lower(point[0], r):
             ok_points = ok_points + 1
 
     field = ok_points / points_count
-    pi = field / (0.5 ** 2)
-    print(pi)
+    pi = field / (r ** 2)
+    f1.label = 'pi = ' + str(pi)
+    f2.label = 'field = ' + str(field)
+    gd.height = 450
+    gd.width = 450
 
 
-def circle_point_lower(x):
-    return (1 - 2 * sqrt(x - x ** 2)) / 2
+def circle_point_lower(x, r):
+    return r - sqrt(2 * r * x - x ** 2)
 
 
-def circle_point_upper(x):
-    return (2 * sqrt(x - x ** 2) + 1) / 2
+def circle_point_upper(x, r):
+    return sqrt(2 * r * x - x ** 2) + r
 
 
 if __name__ == '__main__':
